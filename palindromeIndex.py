adcc8c0be1a2c637f9813e544fce513f8a99a640
tombudny <outofastem@gmail.com>
2022-06-28 15:03:39 -0400

    practice for test

--- palindromeIndex.py
+++ palindromeIndex.py
@@ -3,18 +3,18 @@
 #  Returns index of any letter whose removal would leave a palindrome, or -1  if no such index exists.
 
 def palindromeIndex(s):
-    def isPalindrom(sub):
+    def isPalindrome(sub):
         '''Recursive palindrome test'''
         # Stopping condition: strings of length 0 or 1 are palindromes.
         if len(sub) <= 1:
             return True
         elif sub[0] == sub[-1]:
-            return isPalindrom(sub[1:-1])
+            return isPalindrome(sub[1:-1])
         else:
             return False
     
     for i in range(len(s)):
-        if isPalindrom(s[0:i]+s[i+1:]):
+        if isPalindrome(s[0:i]+s[i+1:]):
             return i    
     return -1
 
@@ -25,4 +25,4 @@ for t in tests:
     s, expected = t
     print(s, ', expected, got', expected, palindromeIndex(s))
 
-#print('abcd', isPalindrom('abcd'))
+#print('abcd', isPalindrome('abcd'))
